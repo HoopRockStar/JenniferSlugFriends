a8bff3ae186f3af7fb48cf7b363bc2b8b959cd89
ZeroLinux <maganazero5@gmail.com>
2012-11-27 17:50:49 -0800

    minor touch ups

--- models/menu.py
+++ models/menu.py
@@ -22,5 +22,5 @@ response.google_analytics_id = None
 #########################################################################
 
 response.menu = [
-    (T('Home'), False, URL('default','index'), [])
+    (T('Home'), False, URL('default','home'), [])
     ]
